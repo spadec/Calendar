fb91323dbe233ad47c0864246b097f6ea69ebda9
Paul Starkov <is5202-1@mail.ru>
2021-07-25 02:35:54 +0600

    Первый билд

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,15 @@ namespace Calendar
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.weekendSecondDay = new System.Windows.Forms.RadioButton();
+            this.weekendFirstDay = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.workSecondDay = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.workFirstDay = new System.Windows.Forms.RadioButton();
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.weekendFirstDay = new System.Windows.Forms.RadioButton();
-            this.weekendSecondDay = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,6 +55,37 @@ namespace Calendar
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(327, 76);
             this.panel1.TabIndex = 1;
+            // 
+            // weekendSecondDay
+            // 
+            this.weekendSecondDay.AutoSize = true;
+            this.weekendSecondDay.Location = new System.Drawing.Point(189, 49);
+            this.weekendSecondDay.Name = "weekendSecondDay";
+            this.weekendSecondDay.Size = new System.Drawing.Size(92, 19);
+            this.weekendSecondDay.TabIndex = 5;
+            this.weekendSecondDay.TabStop = true;
+            this.weekendSecondDay.Text = "второй день";
+            this.weekendSecondDay.UseVisualStyleBackColor = true;
+            // 
+            // weekendFirstDay
+            // 
+            this.weekendFirstDay.AutoSize = true;
+            this.weekendFirstDay.Location = new System.Drawing.Point(189, 24);
+            this.weekendFirstDay.Name = "weekendFirstDay";
+            this.weekendFirstDay.Size = new System.Drawing.Size(95, 19);
+            this.weekendFirstDay.TabIndex = 4;
+            this.weekendFirstDay.TabStop = true;
+            this.weekendFirstDay.Text = "первый день";
+            this.weekendFirstDay.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Сегодня я отдыхаю:";
             // 
             // workSecondDay
             // 
@@ -94,7 +126,7 @@ namespace Calendar
             this.monthCalendar1.CausesValidation = false;
             this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Monday;
             this.monthCalendar1.ForeColor = System.Drawing.SystemColors.Info;
-            this.monthCalendar1.Location = new System.Drawing.Point(7, 90);
+            this.monthCalendar1.Location = new System.Drawing.Point(7, 104);
             this.monthCalendar1.MinDate = new System.DateTime(1999, 12, 31, 0, 0, 0, 0);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 3;
@@ -103,50 +135,29 @@ namespace Calendar
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(223, 264);
+            this.button1.Location = new System.Drawing.Point(223, 273);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(112, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Посчитать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // label3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 6);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 15);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Сегодня я отдыхаю:";
-            // 
-            // weekendFirstDay
-            // 
-            this.weekendFirstDay.AutoSize = true;
-            this.weekendFirstDay.Location = new System.Drawing.Point(189, 24);
-            this.weekendFirstDay.Name = "weekendFirstDay";
-            this.weekendFirstDay.Size = new System.Drawing.Size(95, 19);
-            this.weekendFirstDay.TabIndex = 4;
-            this.weekendFirstDay.TabStop = true;
-            this.weekendFirstDay.Text = "первый день";
-            this.weekendFirstDay.UseVisualStyleBackColor = true;
-            // 
-            // weekendSecondDay
-            // 
-            this.weekendSecondDay.AutoSize = true;
-            this.weekendSecondDay.Location = new System.Drawing.Point(189, 49);
-            this.weekendSecondDay.Name = "weekendSecondDay";
-            this.weekendSecondDay.Size = new System.Drawing.Size(92, 19);
-            this.weekendSecondDay.TabIndex = 5;
-            this.weekendSecondDay.TabStop = true;
-            this.weekendSecondDay.Text = "второй день";
-            this.weekendSecondDay.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "*Жирным, отмеченны рабочие смены";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(347, 302);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.monthCalendar1);
             this.Controls.Add(this.panel1);
@@ -156,6 +167,7 @@ namespace Calendar
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -169,6 +181,7 @@ namespace Calendar
         private System.Windows.Forms.RadioButton weekendSecondDay;
         private System.Windows.Forms.RadioButton weekendFirstDay;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,21 +19,7 @@ namespace Calendar
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-            DateTime today = DateTime.Today;
-            DateTime today2 = DateTime.Today;
-            DateTime[] forward = new DateTime[365];
-            DateTime[] forward2 = new DateTime[365];
-            for (var i = 0; i <= 165; i++){
-                forward[i] = today.AddDays(3);
-                today = today.AddDays(4);
-            }
-            for (var j = 0; j<= 165; j++)
-            {
-                forward2[j] = today2.AddDays(4);
-                today2 = today2.AddDays(4);
-            }
-            monthCalendar1.BoldedDates = forward.Concat(forward2).ToArray();
+            //this.recalculateCalendar(3, 4, -3, -4);
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
@@ -48,7 +34,58 @@ namespace Calendar
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            monthCalendar1.BoldedDates = new DateTime[] { };
+            if (workFirstDay.Checked)
+            {
+                this.recalculateCalendar(0, 1, -3, -4);
+            }
+            if (workSecondDay.Checked)
+            {
+                this.recalculateCalendar(0, 3, -1, -4);
+            }
+            if (weekendFirstDay.Checked)
+            {
+                this.recalculateCalendar(2, 3, -1, -2);
+            }
+            if (weekendSecondDay.Checked)
+            {
+                this.recalculateCalendar(1,2,-2,-3);
+            }
+        }
+        private void recalculateCalendar(int firstDate, int secondDate, int firstBack, int secondBack)
+        {
+            DateTime today = DateTime.Today;
+            DateTime today2 = DateTime.Today;
+            DateTime b_today = DateTime.Today;
+            DateTime b_today2 = DateTime.Today;
+            DateTime[] forward = new DateTime[365];
+            DateTime[] forward2 = new DateTime[365];
+            DateTime[] backward = new DateTime[365];
+            DateTime[] backward2 = new DateTime[365];
+            for (var i = 0; i <= 165; i++)
+            {
+                forward[i] = today.AddDays(firstDate);
+                today = today.AddDays(4);
+            }
+            for (var j = 0; j <= 165; j++)
+            {
+                forward2[j] = today2.AddDays(secondDate);
+                today2 = today2.AddDays(4);
+            }
+            for(var k =0; k<=165; k++)
+            {
+                backward[k] = b_today.AddDays(firstBack);
+                b_today = b_today.AddDays(-4);
+            }
+            for (var p = 0; p <= 165; p++)
+            {
+                backward2[p] = b_today2.AddDays(secondBack);
+                b_today2 = b_today2.AddDays(-4);
+            }
+            //monthCalendar1.BoldedDates = forward.Concat(forward2).ToArray();
+            DateTime[] fullForward = forward.Concat(forward2).ToArray();
+            DateTime[] fullBackward = backward.Concat(backward2).ToArray();
+            monthCalendar1.BoldedDates = fullForward.Concat(fullBackward).ToArray();
         }
     }
 }
